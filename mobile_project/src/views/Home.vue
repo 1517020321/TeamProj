3d614861c80722e8ad7850dde18e89f75a0fcad5
1517020321 <34504737+1517020321@users.noreply.github.com>
2020-04-16 10:08:53 +0800

    11
    11

--- mobile_project/src/views/Home.vue
+++ mobile_project/src/views/Home.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
      <p>我们的项目</p>
+      <button>我是按钮</button>
   </div>
 </template>
 <style  scoped>
-
+p {
+  text-align: center;
+  margin-top: 0.2rem;
+  font-size: 0.14rem;
+}
+button {
+  width: 3rem;
+  height: 0.3rem;
+  font-size: 0.14rem;
+  line-height: 0.3rem;
+  display: block;
+  margin: 0 auto;
+  margin-top:0.21rem;
+}
 </style>
 <script>
 export default {
